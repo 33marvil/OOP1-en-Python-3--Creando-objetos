eafe07c33649deab5915596577dc1b9e1e336c35
33marvil <martinramirezcruz71@gmail.com>
2018-07-21 12:07:14 -0500

    OOP1 crenado objetos version 1

---
+++ objetos-en-python3.py
@@ -0,0 +1,31 @@
+"""dog class"""
+class Dog:
+    pass
+
+#dog objects
+race = Dog()
+age = Dog()
+
+"""people class"""
+class People:
+    pass
+
+#people objects
+gender = People()
+age = People()
+
+"""animal class"""
+class Animal:
+    pass
+
+#animal objects
+species = Animal()
+size = Animal()
+
+"""book class"""
+class Book:
+    pass
+
+#book objects
+type_gender = Book()
+author = Book()
